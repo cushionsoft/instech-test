74d837dd7a558ad0bac9eef9dd7d772a3187fd7d
Andrei Karakouski <a.karakouski@godeltech.com>
2024-03-18 00:00:06 +0100

    Add ClaimServiceTests and CoverServiceTests

--- Claims.Tests/Services/ClaimServiceTests.cs
+++ Claims.Tests/Services/ClaimServiceTests.cs
@@ -8,21 +8,29 @@ namespace Claims.Application.Services.Tests
 {
     public class ClaimServiceTests
     {
+        private readonly IAuditService _auditService;
+        private readonly IClaimRepository _claimRepository;
+        private readonly ClaimService _claimService;
+
+        public ClaimServiceTests()
+        {
+            _auditService = Substitute.For<IAuditService>();
+            _claimRepository = Substitute.For<IClaimRepository>();
+            _claimService = new ClaimService(_auditService, _claimRepository);
+        }
+
         [Fact]
         public async Task CreateAsync_Should_CreateClaimAndAddAudit()
         {
             // Arrange
             var claim = new Claim { Id = "1", CoverId = "1", Created = DateTime.Now, DamageCost = 1, Name = "Test Claim", Type = Core.Enums.ClaimType.BadWeather };
-            var auditService = Substitute.For<IAuditService>();
-            var claimRepository = Substitute.For<IClaimRepository>();
-            var claimService = new ClaimService(auditService, claimRepository);
 
             // Act
-            var result = await claimService.CreateAsync(claim);
+            var result = await _claimService.CreateAsync(claim);
 
             // Assert
-            await claimRepository.Received().AddItemAsync(claim);
-            auditService.Received().AddClaimAudit(Arg.Is<ClaimAudit>(audit => audit.ClaimId == claim.Id && audit.HttpRequestType == "POST"));
+            await _claimRepository.Received().AddItemAsync(claim);
+            _auditService.Received().AddClaimAudit(Arg.Is<ClaimAudit>(audit => audit.ClaimId == claim.Id && audit.HttpRequestType == "POST"));
             Assert.Equal(claim, result);
         }
 
@@ -31,16 +39,13 @@ namespace Claims.Application.Services.Tests
         {
             // Arrange
             var claimId = "1";
-            var auditService = Substitute.For<IAuditService>();
-            var claimRepository = Substitute.For<IClaimRepository>();
-            var claimService = new ClaimService(auditService, claimRepository);
 
             // Act
-            await claimService.DeleteAsync(claimId);
+            await _claimService.DeleteAsync(claimId);
 
             // Assert
-            await claimRepository.Received().DeleteItemAsync(claimId);
-            auditService.Received(1).AddClaimAudit(Arg.Is<ClaimAudit>(audit => audit.ClaimId == claimId && audit.HttpRequestType == "DELETE"));
+            await _claimRepository.Received().DeleteItemAsync(claimId);
+            _auditService.Received(1).AddClaimAudit(Arg.Is<ClaimAudit>(audit => audit.ClaimId == claimId && audit.HttpRequestType == "DELETE"));
         }
     }
 }
---
+++ Claims.Tests/Services/CoverServiceTests.cs
@@ -0,0 +1,62 @@
+﻿using Claims.Core.Entities;
+using Claims.Core.Repositories;
+using Claims.Core.Services;
+using NSubstitute;
+using NSubstitute.Extensions;
+using Xunit;
+
+namespace Claims.Application.Services.Tests
+{
+    public class CoverServiceTests
+    {
+        private readonly IAuditService _auditService;
+        private readonly ICoverRepository _coverRepository;
+        private readonly IPremiumCalculatorService _premiumCalculatorService;
+        private readonly CoverService _coverService;
+
+        public CoverServiceTests()
+        {
+            _auditService = Substitute.For<IAuditService>();
+            _coverRepository = Substitute.For<ICoverRepository>();
+            _premiumCalculatorService = Substitute.For<IPremiumCalculatorService>();
+            _premiumCalculatorService.ReturnsForAll<decimal>(1);
+            _coverService = new CoverService(_auditService, _premiumCalculatorService, _coverRepository);
+        }
+
+        [Fact]
+        public async Task CreateAsync_Should_CreateCoverAndAddAudit()
+        {
+            // Arrange
+            var cover = new Cover
+            {
+                Id = "1",
+                StartDate = DateOnly.FromDateTime(DateTime.Now),
+                EndDate = DateOnly.FromDateTime(DateTime.Now),
+                Type = Core.Enums.CoverType.Yacht,
+                Premium = 1
+            };
+
+            // Act
+            var result = await _coverService.CreateAsync(cover);
+
+            // Assert
+            await _coverRepository.Received().AddItemAsync(cover);
+            _auditService.Received().AddCoverAudit(Arg.Is<CoverAudit>(audit => audit.CoverId == cover.Id && audit.HttpRequestType == "POST"));
+            Assert.Equal(cover, result);
+        }
+
+        [Fact]
+        public async Task DeleteAsync_Should_DeleteCoverAndAddAudit()
+        {
+            // Arrange
+            var coverId = "1";
+
+            // Act
+            await _coverService.DeleteAsync(coverId);
+
+            // Assert
+            await _coverRepository.Received().DeleteItemAsync(coverId);
+            _auditService.Received(1).AddCoverAudit(Arg.Is<CoverAudit>(audit => audit.CoverId == coverId && audit.HttpRequestType == "DELETE"));
+        }
+    }
+}
